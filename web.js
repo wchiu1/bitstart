1399735dbb4bf1a97554c4436df21c609143c581
wchiu1 <wchiu1@inside.artcenter.edu>
2013-08-26 21:50:14 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var port = process.env.PORT || 5000;
+
 app.use(express.logger());
 
+var data = new Buffer(fs.readFileSync('index.html'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
